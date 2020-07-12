f1a919475d4f51a9e207ba97f8020af1f98affea
Arnaud Esteve <arnaud.esteve@gmail.com>
2020-07-12 09:17:48 +0200

    remove macro use in chat messages

--- src/blocks/chat.rs
+++ src/blocks/chat.rs
@@ -1,5 +1,6 @@
 use crate::blocks::gameblock::GameBlock;
 use crate::utils::zero_terminated;
+use nom::bytes::complete::take;
 use nom::{
     number::complete::{le_u16, le_u8},
     IResult,
@@ -44,49 +45,40 @@ impl Addressee {
     }
 }
 
-named!(
-    parse_message<&[u8], ChatMsgBlock>,
-    switch!(take!(1),
-        [16] => call!(startup_chat_msg) |
-        [32] => call!(addressee_chat_msg) |
-        _ => call!(unknown_chat_msg)
-    )
-);
-
-fn startup_chat_msg(input: &[u8]) -> IResult<&[u8], ChatMsgBlock> {
-    do_parse!(input, (ChatMsgBlock::StartupMsg))
+fn parse_message(input: &[u8]) -> IResult<&[u8], ChatMsgBlock> {
+    let (rest, msg_type) = le_u8(input)?;
+    match msg_type {
+        16 => Ok((rest, ChatMsgBlock::StartupMsg)),
+        32 => addressee_chat_msg(rest),
+        _ => Ok((rest, ChatMsgBlock::Unknown)),
+    }
 }
 
 fn addressee_chat_msg(input: &[u8]) -> IResult<&[u8], ChatMsgBlock> {
-    do_parse!(
-        input,
-        to: take!(4) >> (ChatMsgBlock::Msg(Addressee::from_u8(&to)))
-    )
+    let (rest, to) = take(4usize)(input)?;
+    Ok((rest, ChatMsgBlock::Msg(Addressee::from_u8(&to))))
 }
 
-fn unknown_chat_msg(input: &[u8]) -> IResult<&[u8], ChatMsgBlock> {
-    do_parse!(input, (ChatMsgBlock::Unknown))
+fn parse_msg_metadata(input: &[u8]) -> IResult<&[u8], PlayerChatMsgMetadata> {
+    let (rest, player_id) = le_u8(input)?;
+    let (rest, byte_count) = le_u16(rest)?;
+    Ok((
+        rest,
+        PlayerChatMsgMetadata {
+            player_id,
+            byte_count,
+        },
+    ))
 }
 
 pub(crate) fn player_chat_msg(input: &[u8]) -> IResult<&[u8], GameBlock> {
-    let (input, meta) = do_parse!(
-        input,
-        player_id: le_u8
-            >> byte_count: le_u16
-            >> (PlayerChatMsgMetadata {
-                player_id,
-                byte_count,
-            })
-    )?;
-    let (input, msg) = parse_message(input)?;
-    let (input, text) = do_parse!(
-        input,
-        b: zero_terminated >> (String::from_utf8_lossy(b).to_string())
-    )?;
+    let (rest, meta) = parse_msg_metadata(input)?;
+    let (rest, msg) = parse_message(rest)?;
+    let (rest, text) = zero_terminated(rest)?;
     let block = GameBlock::PlayerChatMsg(PlayerChatMsgBlock {
         player_id: meta.player_id,
         kind: msg,
-        text,
+        text: String::from_utf8_lossy(text).to_string(),
     });
-    Ok((input, block))
+    Ok((rest, block))
 }
