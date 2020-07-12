e9b13b55bc7846f98a17cfe5ba86d1169621c2a6
Arnaud Esteve <arnaud.esteve@gmail.com>
2020-07-12 12:42:30 +0200

    remove macro use in replay metadata

--- src/metadata/replay.rs
+++ src/metadata/replay.rs
@@ -1,4 +1,5 @@
 use crate::utils::zero_terminated;
+use nom::bytes::complete::{tag, take};
 use nom::{
     number::complete::{le_u16, le_u32},
     IResult,
@@ -27,43 +28,43 @@ pub(crate) struct ReplayMetaData {
 }
 
 pub(crate) fn parse_header(input: &[u8]) -> IResult<&[u8], FileMetaData> {
-    do_parse!(
-        input,
-        ignored: tag!(REPLAY_PREFIX)
-            >> magic: zero_terminated
-            >> offset: le_u32
-            >> compressed_size: le_u32
-            >> header_version: take!(4u8)
-            >> decompressed_size: le_u32
-            >> compressed_data_block_count: le_u32
-            >> replay_metadata: parse_replay_metadata
-            >> (FileMetaData {
-                offset,
-                compressed_size,
-                header_version: String::from_utf8_lossy(header_version).to_string(),
-                decompressed_size,
-                compressed_data_block_count,
-                replay_metadata,
-            })
-    )
+    let (rest, _) = tag(REPLAY_PREFIX)(input)?;
+    let (rest, _) = zero_terminated(rest)?; // "magic" string
+    let (rest, offset) = le_u32(rest)?;
+    let (rest, compressed_size) = le_u32(rest)?;
+    let (rest, header_version) = take(4usize)(rest)?;
+    let (rest, decompressed_size) = le_u32(rest)?;
+    let (rest, compressed_data_block_count) = le_u32(rest)?;
+    let (rest, replay_metadata) = parse_replay_metadata(rest)?;
+    Ok((
+        rest,
+        FileMetaData {
+            offset,
+            compressed_size,
+            header_version: String::from_utf8_lossy(header_version).to_string(),
+            decompressed_size,
+            compressed_data_block_count,
+            replay_metadata,
+        },
+    ))
 }
 
 fn parse_replay_metadata(input: &[u8]) -> IResult<&[u8], ReplayMetaData> {
-    do_parse!(
-        input,
-        game_identifier: take!(4)
-            >> version: le_u32
-            >> build_no: le_u16
-            >> flags: take!(2)
-            >> replay_length_ms: le_u32
-            >> checksum: le_u32
-            >> (ReplayMetaData {
-                game_identifier: String::from_utf8_lossy(game_identifier).to_string(),
-                version,
-                build_no,
-                flags: String::from_utf8_lossy(flags).to_string(),
-                replay_length_ms,
-                checksum
-            })
-    )
+    let (rest, game_identifier) = take(4usize)(input)?;
+    let (rest, version) = le_u32(rest)?;
+    let (rest, build_no) = le_u16(rest)?;
+    let (rest, flags) = take(2usize)(rest)?;
+    let (rest, replay_length_ms) = le_u32(rest)?;
+    let (rest, checksum) = le_u32(rest)?;
+    Ok((
+        rest,
+        ReplayMetaData {
+            game_identifier: String::from_utf8_lossy(game_identifier).to_string(),
+            version,
+            build_no,
+            flags: String::from_utf8_lossy(flags).to_string(),
+            replay_length_ms,
+            checksum,
+        },
+    ))
 }
