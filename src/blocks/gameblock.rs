54cbc92447db332d1199c940867992dec0ca6a1a
Arnaud Esteve <arnaud.esteve@gmail.com>
2020-07-12 12:11:04 +0200

    remove macro use in game block

--- src/blocks/gameblock.rs
+++ src/blocks/gameblock.rs
@@ -1,5 +1,8 @@
 use crate::blocks::chat::{player_chat_msg, PlayerChatMsgBlock};
 use crate::blocks::command::{parse_command, CommandData};
+use nom::bytes::complete::take;
+use nom::combinator::map_res;
+use nom::multi::many0;
 use nom::{
     number::complete::{le_u16, le_u8},
     IResult,
@@ -61,79 +64,70 @@ pub(crate) fn parse_time_blocks(input: &[u8]) -> IResult<&[u8], Option<CommandDa
     }
 }
 
-named!(
-    parsed_game_block<&[u8], GameBlock>,
-    switch!(take!(1),
-        [23] => dbg!(call!(parse_leave_block)) |
-        [26] => call!(ignore_4) | // 1st start
-        [27] => call!(ignore_4) | // 2nd start
-        [28] => call!(ignore_4) | // 3rd start
-        [30] => call!(time_slot_block) |
-        [31] => call!(time_slot_block) |
-        [32] => call!(player_chat_msg) |
-        [34] => call!(unknown_022) |
-        [35] => call!(unknown_023) |
-        [47]=> call!(forced_game_end_cd) |
-        _ => value!(GameBlock::Unknown)
-    )
-);
-
-named!(
-    parsed_game_blocks<&[u8], (Vec<GameBlock>, &[u8])>,
-    many_till!(parsed_game_block, eof!())
-);
-
 pub(crate) fn parse_game_blocks(input: &[u8]) -> IResult<&[u8], Vec<GameBlock>> {
-    let (input, (res, _)) = parsed_game_blocks(input)?;
-    Ok((input, res))
+    many0(parsed_game_block)(input)
+}
+
+fn parsed_game_block(input: &[u8]) -> IResult<&[u8], GameBlock> {
+    let (rest, kind) = le_u8(input)?;
+    match kind {
+        23 => parse_leave_block(rest),
+        26 | 27 => ignore_4(rest),
+        30 | 31 => time_slot_block(rest),
+        32 => player_chat_msg(rest),
+        34 => unknown_022(rest),
+        35 => unknown_023(rest),
+        47 => forced_game_end_cd(rest),
+        _ => Ok((rest, GameBlock::Unknown)),
+    }
 }
 
 fn unknown_022(input: &[u8]) -> IResult<&[u8], GameBlock> {
-    do_parse!(
-        input,
-        length: le_u8 >> skipped: dbg!(take!(length)) >> (GameBlock::Unknown)
-    )
+    let (rest, length) = le_u8(input)?;
+    let (rest, _) = take(length as usize)(rest)?;
+    Ok((rest, GameBlock::Unknown))
 }
 
 fn unknown_023(input: &[u8]) -> IResult<&[u8], GameBlock> {
-    do_parse!(input, skipped: take!(8) >> (GameBlock::Unknown))
+    let (rest, _) = take(8usize)(input)?;
+    Ok((rest, GameBlock::Unknown))
 }
 
 fn forced_game_end_cd(input: &[u8]) -> IResult<&[u8], GameBlock> {
-    do_parse!(input, skipped: take!(8) >> (GameBlock::Unknown))
+    let (rest, _) = take(8usize)(input)?;
+    Ok((rest, GameBlock::Unknown))
 }
 
 fn time_slot_block(input: &[u8]) -> IResult<&[u8], GameBlock> {
-    let (input, res) = do_parse!(
-        input,
-        byte_count: le_u16
-            >> time_increment: le_u16
-            >> command: map_res!(take!(byte_count - 2), parse_time_blocks)
-            >> (GameBlock::TimeSlot(TimeSlotBlock {
-                byte_count,
-                time_increment,
-                command: command.1
-            }))
-    )?;
-    Ok((input, res))
+    let (rest, byte_count) = le_u16(input)?;
+    let (rest, time_increment) = le_u16(rest)?;
+    let (rest, command) = map_res(take((byte_count - 2) as usize), parse_time_blocks)(rest)?;
+    Ok((
+        rest,
+        GameBlock::TimeSlot(TimeSlotBlock {
+            byte_count,
+            time_increment,
+            command: command.1,
+        }),
+    ))
 }
 
 fn parse_leave_block(input: &[u8]) -> IResult<&[u8], GameBlock> {
-    let (input, res) = do_parse!(
-        input,
-        reason: take!(4)
-            >> player_id: le_u8
-            >> result: take!(4)
-            >> ignored: take!(4)
-            >> (GameBlock::Leave(LeaveGameBlock {
-                player_id,
-                reason: reason[0..4].try_into().unwrap(),
-                result: result[0..4].try_into().unwrap(),
-            }))
-    )?;
-    Ok((input, res))
+    let (rest, reason) = take(4usize)(input)?;
+    let (rest, player_id) = le_u8(rest)?;
+    let (rest, result) = take(4usize)(rest)?;
+    let (rest, _) = take(4usize)(rest)?;
+    Ok((
+        rest,
+        GameBlock::Leave(LeaveGameBlock {
+            player_id,
+            reason: reason[0..4].try_into().unwrap(),
+            result: result[0..4].try_into().unwrap(),
+        }),
+    ))
 }
 
 fn ignore_4(input: &[u8]) -> IResult<&[u8], GameBlock> {
-    do_parse!(input, skipped: take!(4) >> (GameBlock::Ignored))
+    let (rest, _) = take(4usize)(input)?;
+    Ok((rest, GameBlock::Ignored))
 }
